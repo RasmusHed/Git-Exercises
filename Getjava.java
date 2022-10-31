f4da7a3d16bef87073a8109ffc7d2a13979ca2eb
Rasmus <rashed1110@skola.goteborg.se>
2022-10-31 14:43:53 +0100

    Version 1.2

--- Getjava.java
+++ Getjava.java
@@ -2,5 +2,6 @@ public class Getjava {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hej Världen!");
+        System.out.println("Hello World Again!");
     }
 }
