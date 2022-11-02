1ae0f05729b5c9a01cd90356db955013d2262d1f
Rasmus <rashed1110@skola.goteborg.se>
2022-11-02 09:31:53 +0100

    adding gitignore

--- Gitjava.java
+++ Gitjava.java
@@ -1,4 +1,4 @@
-public class Getjava {
+public class Gitjava {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hej Världen!");
