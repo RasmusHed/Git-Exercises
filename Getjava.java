fd35b6bfc14288af1e1d7e43c2d10b9635e13478
Rasmus <rashed1110@skola.goteborg.se>
2022-11-16 10:31:10 +0100

    remove useless stuff

--- Getjava.java
+++
@@ -1,8 +0,0 @@
-public class Getjava {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hej Världen!");
-        System.out.println("Hello World Again!");
-        System.out.println("Hej Världen igen!");
-    }
-}
